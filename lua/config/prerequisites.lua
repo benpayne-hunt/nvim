6aa5fb8a9c39d1f69a8d51c7cbdcfd5417ed821f
Ben Payne Hunt <ben@benpaynehunt.co.uk>
2025-06-10 22:46:20 +0100

    Updates to new version

--- lua/config/prerequisites.lua
+++ lua/config/prerequisites.lua
@@ -1,13 +1,16 @@
 local dependencies = {
-	"gcc",
-	"rg",
-	"composer",
-	"node",
-	"npm",
+  "gcc",
+  "rg",
+  "composer",
+  "node",
+  "npm",
+  "lazygit",
+  "nvr",
+  "cargo",
 }
 
 for index, dependency in pairs(dependencies) do
-	if vim.fn.executable(dependency) == 0 then
-		print("Missing executable: " .. dependency .. " (" .. index .. ")")
-	end
+  if vim.fn.executable(dependency) == 0 then
+    error("Missing executable: " .. dependency .. " (" .. index .. ")")
+  end
 end
