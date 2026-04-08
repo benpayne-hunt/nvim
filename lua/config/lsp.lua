8dbf6f42363016a14c6fc1f821f80d703dd87c1e
Benjamin Payne Hunt <bphunt.com@gmail.com>
2026-04-08 10:36:47 +0100

    Updates with smear and tmux navigator

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -17,13 +17,6 @@ vim.lsp.config("*", {
             client.server_capabilities.completionProvider.triggerCharacters =
                 characters.all
         end
-
-        vim.lsp.completion.enable(true, client.id, bufnr, {
-            autotrigger = true,
-            convert = function(item)
-                return { abbr = item.label:gsub("%b()", "") }
-            end,
-        })
     end,
 })
 
