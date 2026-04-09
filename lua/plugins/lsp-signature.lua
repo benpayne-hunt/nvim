a021bcc15486afe2818a2b5e9dec67bf0dcb580a
Benjamin Payne Hunt <bphunt.com@gmail.com>
2026-04-09 10:55:45 +0100

    Fix lsp issue

--- lua/plugins/lsp-signature.lua
+++ lua/plugins/lsp-signature.lua
@@ -2,7 +2,6 @@ return {
     "ray-x/lsp_signature.nvim",
     event = "InsertEnter",
     opts = {
-        always_trigger = true,
         floating_window = false,
         hint_prefix = { above = "↙ ", current = "← ", below = "↖ " },
     },
